ff15b40817c5792c31a55661dcf792bce0cbf40b
RashmiPal <43467685+RashmiPal@users.noreply.github.com>
2023-11-30 19:15:17 +0530

    Add files via upload

--- index.mjs
+++ index.mjs
@@ -1,1 +1,10 @@
+// Just having this require breaks jest (without additional config):
+const {} = require("aws-jwt-verify");
 
+const hello = "Hello";
+
+module.exports = { hello };
+
+if (require.main === module) {
+  console.log(hello);
+}
